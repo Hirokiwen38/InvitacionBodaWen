b0d8fde9b30cd2a62111857a2d2c1e249b52da14
Wendolyne <developer4@grupoaguilas.com>
2026-04-01 12:25:30 -0600

    fix

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -94,7 +94,7 @@ document.addEventListener('DOMContentLoaded', () => {
             };
 
             try {
-                const response = await fetch('http://127.0.0.1:5000/confirmar', {
+                const response = await fetch('https://invitacion-boda-wen.vercel.app/confirmar', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify(formData)
